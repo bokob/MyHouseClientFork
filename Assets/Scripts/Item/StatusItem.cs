e39ecc1c6012b0eb80e275afd3221df3920a8641
bokob <bmc0520@naver.com>
2024-05-05 19:13:47 +0900

    [FEAT][ADD] 상태 아이템(체력, 에너지) 에셋 추가 및 기능 구현

---
+++ Assets/Scripts/Item/StatusItem.cs
@@ -0,0 +1,57 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StatusItem : Item
+{
+    [SerializeField]
+    float pickupRange;
+    void Start()
+    {
+        ItemInit();
+        pickupRange = 2f;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Floating();
+    }
+
+    // ªÛ≈¬ æ∆¿Ã≈€ º∑√Î
+    void TakeStatusItem(Collider other)
+    {
+        Status status = other.GetComponent<Status>();
+        if(base.itemType == Define.Item.Heart)
+        {
+            if ((int)status.Hp == (int)status.MaxHp)
+                return;
+            status.Heal();
+        }
+        else if(base.itemType == Define.Item.Energy)
+        {
+            if ((int)status.Sp == (int)status.MaxSp)
+                return;
+            status.SpUp();
+        }
+        Destroy(gameObject);
+    }
+
+    void PickUp(Collider other)
+    {
+        Debug.Log("æ∆¿Ã≈€ »πµÊ");
+        TakeStatusItem(other);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        Debug.Log("π¸¿ßø° µÈæÓø»");
+        PickUp(other);
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(transform.position, pickupRange);
+    }
+}
